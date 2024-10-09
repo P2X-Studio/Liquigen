5d00a044d97070f12e7ea6e3b917fd87c9c1cb48
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-10-09 16:55:15 +0300

    Initialized event listener for KimFactory PairCreated event
    -- This listens for events emitted by the KimFactory Contract after
    creating a new pair.
    -- Not yet implemented the main logic. Just setup the scaffolding.

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,38 @@
 import express from "express";
+import { ethers } from "ethers";
+import { config } from "dotenv";
+import kimFactoryABI from "../smart-contracts/artifacts/contracts/LPNFTFactory.sol/KimLPNFTFactory.json" with { type: "json" };
 
+// Load .env file
+config({
+  path: "../smart-contracts/.env",
+});
+
+// Epress server setup
 const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Im alive!");
 });
 
-app.listen(port, () => {
+// Ethers.js setup to listen for Kim events
+const provider = new ethers.JsonRpcProvider();
+
+// ~~~~~~ Kim Factory Event Listeners ~~~~~~
+const kimFactoryContract = new ethers.Contract(
+  process.env.LPNFTFACTORY_CONTRACT_ADDRESS,
+  kimFactoryABI.abi,
+  provider,
+);
+
+// Listen for Create Pair event
+kimFactoryContract.on("PairCreated", async (res) => {
+  console.log("Pair Created: ", res);
+});
+
+// ~~~~~~ Start the server ~~~~~~
+app.listen(port, async () => {
   console.log(`Server is running on port ${port}`);
+  console.log(await provider.getBlockNumber());
 });
